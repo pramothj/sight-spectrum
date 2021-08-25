e314b8d18eb1ffd4d65c23a1181adee953305018
Tharunraj Optimus <59702671+tharunoptimus@users.noreply.github.com>
2021-08-25 21:03:10 +0530

    Added Event Listener to the script

--- register.php
+++ register.php
@@ -1,47 +1,43 @@
 <?php 
 
-include 'config.php';
+	include 'config.php';
+	error_reporting(0);
+	session_start();
 
-error_reporting(0);
+	if (isset($_SESSION['username'])) {
+		header("Location: index.php");
+	}
 
-session_start();
+	if (isset($_POST['submit'])) {
+		$username = $_POST['username'];
+		$email = $_POST['email'];
+		$password = md5($_POST['password']);
+		$cpassword = md5($_POST['cpassword']);
+			$errorMessage = "";
 
-if (isset($_SESSION['username'])) {
-    header("Location: index.php");
-}
-
-if (isset($_POST['submit'])) {
-	$username = $_POST['username'];
-	$email = $_POST['email'];
-	$password = md5($_POST['password']);
-	$cpassword = md5($_POST['cpassword']);
-        $errorMessage = "";
-
-	if ($password == $cpassword) {
-		$sql = "SELECT * FROM users WHERE email='$email'";
-		$result = mysqli_query($conn, $sql);
-		if (!$result->num_rows > 0) {
-			$sql = "INSERT INTO users (username, email, password)
-					VALUES ('$username', '$email', '$password')";
+		if ($password == $cpassword) {
+			$sql = "SELECT * FROM users WHERE email='$email'";
 			$result = mysqli_query($conn, $sql);
-			if ($result) {
-				echo "<script>alert('Wow! User Registration Completed.')</script>";
-				$username = "";
-				$email = "";
-				$_POST['password'] = "";
-				$_POST['cpassword'] = "";
+			if (!$result->num_rows > 0) {
+				$sql = "INSERT INTO users (username, email, password)
+						VALUES ('$username', '$email', '$password')";
+				$result = mysqli_query($conn, $sql);
+				if ($result) {
+					echo "<script>alert('Wow! User Registration Completed.')</script>";
+					$username = "";
+					$email = "";
+					$_POST['password'] = "";
+					$_POST['cpassword'] = "";
+				} else {
+					$errorMessage = "Woops! Something Wrong Went";
+				}
 			} else {
-				$errorMessage = "Woops! Something Wrong Went";
+					$errorMessage = "Woops! Email Already Exists.";
 			}
 		} else {
-			      $errorMessage = "Woops! Email Already Exists.";
+			$errorMessage = "Password Not Matched.";
 		}
-		
-	} else {
-		   $errorMessage = "Password Not Matched.";
 	}
-}
-
 ?>
 
 <!DOCTYPE html>
@@ -79,9 +75,11 @@ if (isset($_POST['submit'])) {
 		</form>
 	</div>
 <?php
-if($errorMessage != "") { 
-echo "<script defer>alert('" . $errorMessage . "')</script>"; 
-}
+	if($errorMessage != "") { 
+		echo '<script>document.addEventListener("DOMContentLoaded", function() {
+			alert(' . $errorMessage . '
+		});</script>';
+	}
 ?>
 </body>
 </html>
